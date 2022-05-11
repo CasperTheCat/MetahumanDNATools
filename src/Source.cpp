45b2d37f493289e7863a493704321d4d059e55d3
CasperTheCat <zsmyna@gmail.com>
2022-05-11 14:48:48 +1200

    Adding code

---
+++ src/Source.cpp
@@ -0,0 +1,548 @@
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <optional>
+#include "Types.h"
+
+std::optional<std::shared_ptr<std::vector<char>>> ReadToVector(std::ifstream& InStream)
+{
+	if(InStream)
+	{
+		// Get Size
+		InStream.seekg(0, std::ios::end);
+		auto FileSize = InStream.tellg();
+		InStream.seekg(0, std::ios::beg);
+
+		// Create Vector
+		std::shared_ptr<std::vector<char>> Data = std::make_shared<std::vector<char>>();
+		Data->resize(FileSize);
+
+		
+
+		//std::vector<unsigned char>
+		InStream.read(Data->data(), FileSize);
+
+		return Data;
+	}
+
+	return std::nullopt;
+}
+
+uint64_t ReadStringEntry(const char* dataPtr, uint64_t index,  StringEntries& BlockPtr)
+{
+	// Read BoneNames
+	BlockPtr.EntryCount = ToLittle32(dataPtr + index);
+	BlockPtr.Entries.resize(BlockPtr.EntryCount);
+	index += 4;
+
+	for (uint32_t i = 0; i < BlockPtr.EntryCount; ++i)
+	{
+		auto StringLength = ToLittle32(dataPtr + index);
+		index += 4;
+		BlockPtr.Entries[i].StrLen = StringLength;
+		BlockPtr.Entries[i].Src = dataPtr + index;
+		index += StringLength;
+	}
+
+	return index;
+}
+
+uint64_t WriteStringEntry(char* dataPtr, uint64_t index, StringEntries& BlockPtr)
+{
+	// Start with the header value
+	WriteBig32(dataPtr + index, BlockPtr.EntryCount);
+	index += 4;
+
+	for (uint32_t i = 0; i < BlockPtr.EntryCount; ++i)
+	{
+		WriteBig32(dataPtr + index, BlockPtr.Entries[i].StrLen);
+		index += 4;
+		memcpy(dataPtr + index, BlockPtr.Entries[i].Src, BlockPtr.Entries[i].StrLen);
+		index += BlockPtr.Entries[i].StrLen;
+	}
+
+	return index;
+}
+
+uint64_t ReadShortEntry(const char* dataPtr, uint64_t index, ShortEntries& BlockPtr)
+{
+	// Read BoneNames
+	BlockPtr.EntryCount = ToLittle32(dataPtr + index);
+	BlockPtr.Entries.resize(BlockPtr.EntryCount);
+	index += 4;
+
+	for (uint32_t i = 0; i < BlockPtr.EntryCount; ++i)
+	{
+		BlockPtr.Entries[i] = ToLittle16(dataPtr + index);
+		index += 2;
+	}
+
+	return index;
+}
+
+uint64_t WriteShortEntry(char* dataPtr, uint64_t index, ShortEntries& BlockPtr)
+{
+	// Start with the header value
+	WriteBig32(dataPtr + index, BlockPtr.EntryCount);
+	index += 4;
+
+	for (uint32_t i = 0; i < BlockPtr.EntryCount; ++i)
+	{
+		WriteBig16(dataPtr + index, BlockPtr.Entries[i]);
+		index += 2;
+	}
+
+	return index;
+}
+
+uint64_t ReadFloatEntry(const char* dataPtr, uint64_t index, FloatEntries& BlockPtr)
+{
+	// Read BoneNames
+	BlockPtr.EntryCount = ToLittle32(dataPtr + index);
+	BlockPtr.Entries.resize(BlockPtr.EntryCount);
+	index += 4;
+
+	for (uint32_t i = 0; i < BlockPtr.EntryCount; ++i)
+	{
+		auto LocalFloatAsInt = ToLittle32(dataPtr + index);
+
+		BlockPtr.Entries[i] = *(reinterpret_cast<float*>(&LocalFloatAsInt));
+		index += 4;
+	}
+
+	return index;
+}
+
+uint64_t WriteFloatEntry(char* dataPtr, uint64_t index, FloatEntries& BlockPtr)
+{
+	// Start with the header value
+	WriteBig32(dataPtr + index, BlockPtr.EntryCount);
+	index += 4;
+
+	for (uint32_t i = 0; i < BlockPtr.EntryCount; ++i)
+	{
+		WriteBig32(dataPtr + index, BlockPtr.Entries[i]);
+		index += 4;
+	}
+
+	return index;
+}
+
+std::shared_ptr<SuperBlock> ParseDNA(std::shared_ptr<std::vector<char>> DnaPtr)
+{
+	// Create Block
+	auto SuperBlk = std::make_shared<SuperBlock>();
+	SuperBlk->dataPtr = DnaPtr;
+
+	// Create C-type pointer for access
+	auto fdPtr = DnaPtr->data();
+
+	// Index
+	uint64_t index = 0;
+
+
+	// Make Header
+	const Header* HeadBlock = reinterpret_cast<const Header*>(fdPtr);
+	index = sizeof(Header);
+	SuperBlk->Head = HeadBlock;
+
+	
+
+	// Block1
+	std::shared_ptr<Block1> UKBlock1 = std::make_shared<Block1>();
+
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock1);
+	UKBlock1->UnknownValue1 = ToLittle32(fdPtr + index);
+	index += 4;
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock2);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock3);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock4);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock5);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock6);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock7);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock8);
+	index = ReadShortEntry(fdPtr, index, UKBlock1->SubBlock9);
+
+	SuperBlk->Block1 = UKBlock1;
+
+
+
+
+	// Block2
+	std::shared_ptr<Block2> UKBlock2 = std::make_shared<Block2>();
+
+	// Fill the first 24 bytes
+	memcpy(UKBlock2->Unknown1, fdPtr + index, 24);
+	index += 24;
+
+	index = ReadShortEntry(fdPtr, index, UKBlock2->SubBlock1);
+
+	memcpy(UKBlock2->Unknown2, fdPtr + index, 356);
+	index += 356;
+
+	SuperBlk->Block2 = UKBlock2;
+
+
+
+
+	// Block Three has data
+	std::shared_ptr<Block3> UKBlock3 = std::make_shared<Block3>();
+
+
+	// Read BoneNames
+	index = ReadStringEntry(fdPtr, index, UKBlock3->SubBlock1_CTRLNames);
+	index = ReadStringEntry(fdPtr, index, UKBlock3->SubBlock2_ExpressionNames);
+	index = ReadStringEntry(fdPtr, index, UKBlock3->SubBlock3_FacialJointNames);
+	index = ReadStringEntry(fdPtr, index, UKBlock3->SubBlock4_OtherJointNames);
+	index = ReadStringEntry(fdPtr, index, UKBlock3->SubBlock5_OtherStrings);
+	index = ReadStringEntry(fdPtr, index, UKBlock3->SubBlock6_Meshes);
+
+	index = ReadShortEntry(fdPtr, index, UKBlock3->SubBlock7);
+	index = ReadShortEntry(fdPtr, index, UKBlock3->SubBlock8);
+	index = ReadShortEntry(fdPtr, index, UKBlock3->SubBlock9);
+
+	index = ReadFloatEntry(fdPtr, index, UKBlock3->SubBlock10);
+	index = ReadFloatEntry(fdPtr, index, UKBlock3->SubBlock11);
+	index = ReadFloatEntry(fdPtr, index, UKBlock3->SubBlock12);
+
+	index = ReadFloatEntry(fdPtr, index, UKBlock3->SubBlock13_JointOrientationX);
+	index = ReadFloatEntry(fdPtr, index, UKBlock3->SubBlock14_JointOrientationY);
+	index = ReadFloatEntry(fdPtr, index, UKBlock3->SubBlock15_JointOrientationZ);
+
+
+	SuperBlk->Block3 = UKBlock3;
+
+	// Block Four has data
+	std::shared_ptr<Block4> UKBlock4 = std::make_shared<Block4>();
+
+	UKBlock4->Unknown = ToLittle16(fdPtr + index);
+	index += 2;
+
+	index = ReadShortEntry(fdPtr, index, UKBlock4->SubBlock1);
+	index = ReadShortEntry(fdPtr, index, UKBlock4->SubBlock2);
+
+	index = ReadFloatEntry(fdPtr, index, UKBlock4->SubBlock3);
+	index = ReadFloatEntry(fdPtr, index, UKBlock4->SubBlock4);
+	index = ReadFloatEntry(fdPtr, index, UKBlock4->SubBlock5);
+	index = ReadFloatEntry(fdPtr, index, UKBlock4->SubBlock6);
+
+	index = ReadShortEntry(fdPtr, index, UKBlock4->SubBlock7);
+	index = ReadShortEntry(fdPtr, index, UKBlock4->SubBlock8);
+
+	index = ReadFloatEntry(fdPtr, index, UKBlock4->SubBlock9);
+
+	SuperBlk->Block4 = UKBlock4;
+
+	return SuperBlk;
+}
+
+std::shared_ptr<std::vector<char>> SerialiseDna(std::shared_ptr<SuperBlock> BlkPtr)
+{
+	// SuperBlock contains original data. Use it resize
+	std::shared_ptr<std::vector<char>> SerialDna = std::make_shared<std::vector<char>>();
+	SerialDna->resize(BlkPtr->dataPtr->size());
+
+	uint64_t index = 0;
+	auto serialPtr = SerialDna->data();
+
+	// The header is unknown
+	memcpy(serialPtr + index, BlkPtr->Head, sizeof(Header));
+	index += sizeof(Header);
+
+	// Block1 is understood, so reverse it
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock1);
+	WriteBig32(serialPtr + index, BlkPtr->Block1->UnknownValue1);
+	index += 4;
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock2);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock3);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock4);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock5);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock6);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock7);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock8);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block1->SubBlock9);
+
+	// Now Block2
+	memcpy(serialPtr + index, BlkPtr->Block2->Unknown1, 24);
+	index += 24;
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block2->SubBlock1);
+	memcpy(serialPtr + index, BlkPtr->Block2->Unknown2, 356);
+	index += 356;
+
+	// Block3
+	index = WriteStringEntry(serialPtr, index, BlkPtr->Block3->SubBlock1_CTRLNames);
+	index = WriteStringEntry(serialPtr, index, BlkPtr->Block3->SubBlock2_ExpressionNames);
+	index = WriteStringEntry(serialPtr, index, BlkPtr->Block3->SubBlock3_FacialJointNames);
+	index = WriteStringEntry(serialPtr, index, BlkPtr->Block3->SubBlock4_OtherJointNames);
+	index = WriteStringEntry(serialPtr, index, BlkPtr->Block3->SubBlock5_OtherStrings);
+	index = WriteStringEntry(serialPtr, index, BlkPtr->Block3->SubBlock6_Meshes);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block3->SubBlock7);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block3->SubBlock8);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block3->SubBlock9);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block3->SubBlock10);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block3->SubBlock11);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block3->SubBlock12);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block3->SubBlock13_JointOrientationX);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block3->SubBlock14_JointOrientationY);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block3->SubBlock15_JointOrientationZ);
+
+	// Block4
+	WriteBig16(serialPtr + index, BlkPtr->Block4->Unknown);
+	index += 2;
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block4->SubBlock1);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block4->SubBlock2);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block4->SubBlock3);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block4->SubBlock4);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block4->SubBlock5);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block4->SubBlock6);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block4->SubBlock7);
+	index = WriteShortEntry(serialPtr, index, BlkPtr->Block4->SubBlock8);
+	index = WriteFloatEntry(serialPtr, index, BlkPtr->Block4->SubBlock9);
+
+	// Rest
+	memcpy(serialPtr + index, BlkPtr->dataPtr->data() + index, BlkPtr->dataPtr->size() - index);
+
+	return SerialDna;
+}
+
+double CalculateFileDiff(std::shared_ptr<std::vector<char>> FirstDNAData, std::shared_ptr<std::vector<char>> SecondDNAData)
+{
+	auto fdPtr = FirstDNAData->data();
+	auto sdPtr = SecondDNAData->data();
+
+	// Okay, now check the sizes
+	//std::cout << FirstDNAData.size() << " - " << SecondDNAData.size() << std::endl;
+
+	// Byte Overlay
+	auto smallestSize = std::min(FirstDNAData->size(), SecondDNAData->size());
+
+	bool FirstHit = false;
+
+	uint64_t delta = 0;
+	for (uint32_t i = 0; i < smallestSize; ++i)
+	{
+		if (fdPtr[i] == sdPtr[i])
+		{
+			// Do Nothing
+		}
+		else
+		{
+			if (!FirstHit)
+			{
+				FirstHit = true;
+				std::cout << "Difference at " << std::hex << i << std::dec << ". Log:" << std::endl << "\tOriginal: " << std::hex;
+
+				for (uint32_t a = 0; a < 20; ++a)
+				{
+					std::cout << (unsigned char)(fdPtr[i - 10 + a]);
+				}
+
+				std::cout << std::endl << "\tModified: ";
+
+				for (uint32_t a = 0; a < 20; ++a)
+				{
+					std::cout << (unsigned char)(sdPtr[i - 10 + a]);
+				}
+
+				std::cout << std::dec << std::endl;
+
+			}
+			delta += 1;
+
+		}
+	}
+
+	return (delta / double(smallestSize)) * 100;
+
+	//std::cout << "Files deviate by " << std::dec << (delta / double(smallestSize)) * 100 << std::endl;
+}
+
+std::optional<std::shared_ptr<SuperBlock>> ReadDNAFile(std::string path)
+{
+	std::ifstream DNAFile(path, std::ios::ate | std::ios::binary);
+
+	if (DNAFile)
+	{
+		auto DNADataOpt = ReadToVector(DNAFile);
+		if (DNADataOpt)
+		{
+			std::shared_ptr<std::vector<char>> DNAData = DNADataOpt.value();
+
+			auto Returnable = ParseDNA(DNAData);
+
+			return Returnable;
+		}
+	}
+
+	return std::nullopt;
+}
+
+void WriteDNAFile(std::string path, std::shared_ptr<SuperBlock> SuperBlock)
+{
+	std::ofstream OutFile(path, std::ios::binary);
+
+	if (OutFile)
+	{
+		// SuperBlock holds our data
+		auto SuperIo = SerialiseDna(SuperBlock);
+
+		OutFile.write(SuperIo->data(), SuperIo->size());
+	}
+}
+
+void Tests(std::shared_ptr<SuperBlock> SuperBlock)
+{
+	// Modified Joint Positions
+	for (uint32_t i = 0; i < SuperBlock->Block3->SubBlock10.EntryCount; ++i)
+	{
+		SuperBlock->Block3->SubBlock10.Entries[i] += 4.1f;
+	}
+
+	for (uint32_t i = 0; i < SuperBlock->Block3->SubBlock11.EntryCount; ++i)
+	{
+		SuperBlock->Block3->SubBlock11.Entries[i] += 3.1f;
+	}
+
+	for (uint32_t i = 0; i < SuperBlock->Block3->SubBlock12.EntryCount; ++i)
+	{
+		SuperBlock->Block3->SubBlock12.Entries[i] += 5.1f;
+	}
+
+
+
+	//// Modified Joint Positions
+	//for (uint32_t i = 0; i < SuperBlock->Block3->SubBlock13_JointOrientationX.EntryCount; ++i)
+	//{
+	//	SuperBlock->Block3->SubBlock13_JointOrientationX.Entries[i] *= 1.1f;
+	//}
+
+	//for (uint32_t i = 0; i < SuperBlock->Block3->SubBlock14_JointOrientationY.EntryCount; ++i)
+	//{
+	//	SuperBlock->Block3->SubBlock14_JointOrientationY.Entries[i] *= 1.1f;
+	//}
+
+	//for (uint32_t i = 0; i < SuperBlock->Block3->SubBlock15_JointOrientationZ.EntryCount; ++i)
+	//{
+	//	SuperBlock->Block3->SubBlock15_JointOrientationZ.Entries[i] *= 1.1f;
+	//}
+}
+
+int main(int argc, char** argv)
+{
+	if (2 == argc)
+	{
+		auto DnaOpt = ReadDNAFile(argv[1]);
+
+		if (DnaOpt)
+		{
+			auto SuperBlk = DnaOpt.value();
+
+			auto SuperSerial = SerialiseDna(SuperBlk);
+
+			std::cout << "Files deviate by " << std::dec << CalculateFileDiff(SuperBlk->dataPtr, SuperSerial) << "%" << std::endl;
+
+			// Modified with Tests
+			//Tests(SuperBlk);
+
+			WriteDNAFile("test.dna", SuperBlk);
+
+
+			// TestWriting
+
+			for (uint16_t i = 0; i < SuperBlk->Block3->SubBlock10.EntryCount; ++i)
+			{
+				uint16_t Little1 = i;
+
+				// Lookup Bone Name
+				if (Little1 < SuperBlk->Block3->SubBlock3_FacialJointNames.EntryCount)
+				{
+					std::cout << SuperBlk->Block3->SubBlock3_FacialJointNames.Entries[Little1].Src << " (" << Little1 << ")";;
+				}
+				else
+				{
+					std::cout << "BadIndex_" << Little1;
+				}
+
+				std::cout << " -> ";
+
+				// Lookup Bone Name
+				if (Little1 < SuperBlk->Block3->SubBlock10.EntryCount)
+				{
+					std::cout << SuperBlk->Block3->SubBlock10.Entries[Little1] << "\t" <<
+						SuperBlk->Block3->SubBlock11.Entries[Little1] << "\t" <<
+						SuperBlk->Block3->SubBlock12.Entries[Little1];
+				}
+				else
+				{
+					std::cout << "BadIndex_" << Little1;
+				}
+
+
+
+
+
+				std::cout << std::endl;
+
+			}
+
+
+			//for (uint16_t i = 0; i < SuperBlk->Block1->SubBlock3.EntryCount; ++i)
+			//{
+			//	uint16_t Little1 = i;
+			//	uint16_t Little2 = SuperBlk->Block1->SubBlock3.Entries[i];
+
+			//	// Lookup Bone Name
+			//	if (Little1 < SuperBlk->Block3->SubBlock2_ExpressionNames.EntryCount)
+			//	{
+			//		std::cout << SuperBlk->Block3->SubBlock2_ExpressionNames.Entries[Little1].Src << " (" << Little1 << ")";
+			//	}
+			//	else
+			//	{
+			//		std::cout << "BadIndex_" << Little1;
+			//	}
+
+			//	std::cout << " -> ";
+
+			//	// Lookup Bone Name
+			//	if (Little2 < SuperBlk->Block3->SubBlock3_FacialJointNames.EntryCount)
+			//	{
+			//		std::cout << SuperBlk->Block3->SubBlock3_FacialJointNames.Entries[Little2].Src << " (" << Little2 << ")";;
+			//	}
+			//	else
+			//	{
+			//		std::cout << "BadIndex_" << Little2;
+			//	}
+
+			//	std::cout << std::endl;
+
+			//}
+		}
+		else
+		{
+			std::cerr << "File Failed to Read" << std::endl;
+		}
+	}
+	else if (argc > 2)
+	{
+		std::ifstream FirstDNA(argv[1], std::ios::ate | std::ios::binary);
+		std::ifstream SecondDNA(argv[2], std::ios::ate | std::ios::binary);
+
+		if (FirstDNA && SecondDNA)
+		{
+			// Create vectors
+			auto FirstDNADataOpt = ReadToVector(FirstDNA);
+			auto SecondDNADataOpt = ReadToVector(SecondDNA);
+
+			if (FirstDNADataOpt && SecondDNADataOpt)
+			{
+				auto FirstDNAData = FirstDNADataOpt.value();
+				auto SecondDNAData = SecondDNADataOpt.value();
+
+				std::cout << "Files deviate by " << std::dec << CalculateFileDiff(FirstDNAData, SecondDNAData) << "%" << std::endl;
+			}
+		}		
+	}
+	
+	return 0;
+}
---
+++ src/Types.h
@@ -0,0 +1,167 @@
+#pragma once
+#include <cstdint>
+
+inline uint32_t ToLittle32(const void* pValue)
+{
+	auto dataPtr = reinterpret_cast<const unsigned char*>(pValue);
+	uint32_t outValue = (dataPtr[3] << 0) | (dataPtr[2] << 8) | (dataPtr[1] << 16) | (dataPtr[0] << 24);
+	return outValue;
+}
+
+inline uint16_t ToLittle16(const void* pValue)
+{
+	auto dataPtr = reinterpret_cast<const unsigned char*>(pValue);
+	uint16_t outValue = (dataPtr[1] << 0) | (dataPtr[0] << 8);
+	return outValue;
+}
+
+void WriteBig32(void* Dst, uint32_t Value)
+{
+	auto dataPtr = reinterpret_cast<uint32_t*>(Dst);
+	*dataPtr = ToLittle32(&Value);
+}
+
+void WriteBig32(void* Dst, float Value)
+{
+	uint32_t ValueAsU32 = *(reinterpret_cast<uint32_t*>(&Value));
+	auto dataPtr = reinterpret_cast<uint32_t*>(Dst);
+	*dataPtr = ToLittle32(&Value);
+}
+
+void WriteBig16(void* Dst, uint16_t Value)
+{
+	auto dataPtr = reinterpret_cast<uint16_t*>(Dst);
+	*dataPtr = ToLittle16(&Value);
+}
+
+
+
+struct StringEntry
+{
+	uint32_t StrLen;
+	const char* Src;
+	//char String[1];
+};
+
+struct StringEntries
+{
+	uint32_t EntryCount;
+	std::vector<StringEntry> Entries;
+};
+
+struct ShortEntries
+{
+	uint32_t EntryCount;
+	std::vector<uint16_t> Entries;
+};
+
+struct FloatEntries
+{
+	uint32_t EntryCount;
+	std::vector<float> Entries;
+};
+
+struct Header
+{
+	char Unknown[0x6B];
+};
+
+struct Block1
+{
+	ShortEntries SubBlock1; // Really Short
+	uint32_t UnknownValue1;
+	
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock2; // Matches size closest with FacialJointNames
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock3; // Matches size closest with OtherJointNames
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock4; // Matches size closest with Something?
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock5; // Too big for inputs?
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock6; // Too big for OtherStrings?
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock7; // 
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock8;
+
+	// Contains values up to 887 (or FacialJointNames)
+	ShortEntries SubBlock9;
+};
+
+struct Block2
+{
+	char Unknown1[24];
+	ShortEntries SubBlock1; // Same size as OtherJointNames. Values are the indices?
+	char Unknown2[356];
+};
+
+struct Block3
+{
+	StringEntries SubBlock1_CTRLNames;
+
+	// Listed as inputs in Maya rl4Embedded
+	StringEntries SubBlock2_ExpressionNames; 
+
+	// Named in Maya, but not listed in rl4
+	StringEntries SubBlock3_FacialJointNames;
+
+	// Listed as Bs outputs in Maya
+	StringEntries SubBlock4_OtherJointNames;
+
+	// Listed as Am outputs in Maya 
+	StringEntries SubBlock5_OtherStrings;
+
+	// List of Maya meshes
+	StringEntries SubBlock6_Meshes;
+
+	ShortEntries SubBlock7;
+	ShortEntries SubBlock8;
+	ShortEntries SubBlock9;
+
+	FloatEntries SubBlock10; // X
+	FloatEntries SubBlock11; // Y
+	FloatEntries SubBlock12; // Z
+
+	FloatEntries SubBlock13_JointOrientationX; // X
+	FloatEntries SubBlock14_JointOrientationY; // Y
+	FloatEntries SubBlock15_JointOrientationZ; // Z
+
+};
+
+struct Block4
+{
+	uint16_t Unknown;
+	ShortEntries SubBlock1;
+	ShortEntries SubBlock2;
+
+	FloatEntries SubBlock3;
+	FloatEntries SubBlock4;
+	FloatEntries SubBlock5; 
+	FloatEntries SubBlock6;
+
+	ShortEntries SubBlock7;
+	ShortEntries SubBlock8;
+
+	FloatEntries SubBlock9;
+};
+
+
+struct SuperBlock
+{
+	const Header* Head;
+	std::shared_ptr<Block1> Block1;
+	std::shared_ptr<Block2> Block2;
+	std::shared_ptr<Block3> Block3;
+	std::shared_ptr<Block4> Block4;
+	
+	std::shared_ptr<std::vector<char>> dataPtr;
+	uint64_t dbgIndex;
+};
